87ed981720848e22a4729e62482a30394a9a2e1a
zerK4 <sebastian.v.pavel@gmail.com>
2024-05-21 21:50:32 +0300

    update remove from favorites notification

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -2,13 +2,18 @@
 
 import { toast } from "sonner";
 import { Button } from "./ui/button";
-import { XIcon } from "lucide-react";
+import { Info, InfoIcon, XIcon } from "lucide-react";
 import { useProductStore } from "@/store/product.store";
 
 export const removeFavoriteAfterAddingInCart = (t: any, product: Product) => {
   return (
     <div className='bg-zinc-100 text-black rounded-md p-4 w-[22rem] backdrop-blur-sm'>
-      <h1>Do you want to remove the product from favorites now?</h1>
+      <div className='flex items-center gap-2'>
+        <InfoIcon size={24} className='fill-black stroke-white' />
+        <span className='text-sm font-medium'>
+          Do you want to remove the product from favorites now?
+        </span>
+      </div>
       <div className='flex items-center gap-2 w-full justify-end'>
         <Button
           className=''
